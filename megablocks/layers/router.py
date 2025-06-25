819a6e19194e3444e26d283a458a2ae6d6593a02
Gio-CCHAN <54894470+Gio-CCHAN@users.noreply.github.com>
2025-06-25 21:39:08 +0800

    Update router.py

--- megablocks/layers/router.py
+++ megablocks/layers/router.py
@@ -1,3 +1,23 @@
+from megablocks.layers import common
+from megablocks.layers.arguments import Arguments
+import torch
+
+
+# NOTE: To enable end-to-end benchmarking without convergence we
+# support a flag to force the router to assign tokens uniformly
+# across the experts. We do this with a custom autograd operation
+# so that PyTorch still executes the full set of router operation.
+class _UniformExpertAssignment(torch.autograd.Function):
+
+
+    @staticmethod
+    def forward(ctx, x, num_experts):
+        out = torch.arange(x.numel(), dtype=x.dtype, device=x.device)
+        out = torch.remainder(out, num_experts)
+        return out.view(x.shape)
+_uniform_expert_assignment = _UniformExpertAssignment.apply
+
+
 class LearnedRouter(torch.nn.Module):
 
     def __init__(self, args : Arguments):
